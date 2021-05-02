db0cf306994d571dfadfda28c05e5346805b92b2
Kybre <hermanchan10@gmail.com>
2021-05-01 23:26:57 -0700

    scuffed
    + added idle animation assets
    + moved base game assets to ./Prefab

--- GameJam/Assets/Scripts/PlayerMovement.cs
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -87,7 +87,95 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
             {
+<<<<<<< Updated upstream
                 isTouchingWall = true;
+=======
+                isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround); // Defines touching gound
+                isTouchingLeft = Physics2D.OverlapCircle(leftPos.position, checkRadius, whatIsGround);
+                isTouchingRight = Physics2D.OverlapCircle(rightPos.position, checkRadius, whatIsGround);
+
+                // Normal jumping
+
+                if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+                {
+                    isJumping = true;
+                    jumpTimeCounter = jumpTime;
+                    rb.velocity = Vector2.up * jumpForce;
+                    FindObjectOfType<OyxgenManager>().jumpDeplete();
+                }
+
+                if (Input.GetKey(KeyCode.Space) && isJumping)
+                {
+                    if (jumpTimeCounter > 0)
+                    {
+                        rb.velocity = Vector2.up * jumpForce;
+                        jumpTimeCounter -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        isJumping = false;
+                    }
+
+                }
+
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    isJumping = false;
+                }
+
+                if (rb.velocity.y > 0)
+                {
+                    rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+                }
+
+                // Coyote Time
+                if (isGrounded)
+                {
+                    coyoteCounter = coyoteTime;
+                }
+
+                if (!isJumping && !isGrounded) {
+                    coyoteCounter -= Time.deltaTime;
+                }
+
+                if (!isJumping && !isGrounded && coyoteCounter > 0 && Input.GetKeyDown(KeyCode.Space)) {
+                    isJumping = true;
+                    jumpTimeCounter = jumpTime;
+                    rb.velocity = Vector2.up * jumpForce;
+                    FindObjectOfType<OyxgenManager>().jumpDeplete();
+                }
+
+
+                // Wall jumping
+
+                if (!isGrounded && (isTouchingLeft || isTouchingRight)) // Wall touching detection
+                {
+                    if (Input.GetAxisRaw("Horizontal") > 0 && lastSurface != whatIsGround || 
+                        Input.GetAxisRaw("Horizontal") < 0 && lastSurface != whatIsGround) // Basically you can only wall jump if you're holding into the wall
+                    {
+                        isTouchingWall = true;
+                    }
+                    else
+                    {
+                        isTouchingWall = false;
+                    }
+                }
+                else
+                {
+                    isTouchingWall = false;
+                }
+
+                //Debug.Log(isTouchingWall);
+
+                if (isTouchingWall) // Wall jump logic
+                {
+                    if (Input.GetButtonDown("Jump")) {
+                        wallJumpCounter = wallJumpTime;
+                        rb.velocity = new Vector2(-h * xWallForce * speed * Time.deltaTime, jumpForce + 5);
+                        //if (lastSurface != whatIsGround) lastSurface = whatIsGround;
+                    }
+                }
+>>>>>>> Stashed changes
             }
             else {
                 isTouchingWall = false;
