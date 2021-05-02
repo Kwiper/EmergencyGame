443b7bfdb6d2062ce7d4dadd9ecf80810edcdb1d
Kybre <hermanchan10@gmail.com>
2021-05-02 03:14:47 -0700

    module 1 edits
    - made start area longer
    - added wall buffer to section introducing wall jumping
    - redid lower path for better visual clarity

--- GameJam/Assets/Scripts/KillzoneManager.cs
+++ GameJam/Assets/Scripts/KillzoneManager.cs
@@ -10,7 +10,6 @@ public class KillzoneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("start running");
     }
 
     // Update is called once per frame
