8098b479eef0b7b615217be208f4ae777147a1ad
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2021-05-02 17:58:29 -0700

    Merged Movement, Camera and Enemy Behavior
    -Movement Branch Now Merged With Enemy Behavior Branch
    -Along with New Swanky Camera Controls

--- GameJam/Assets/Scripts/BasicEnemyBehavior.cs
+++ GameJam/Assets/Scripts/BasicEnemyBehavior.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class BasicEnemyBehavior : MonoBehaviour {
 	private Vector2 lastPlayerPos;
 	public GameObject player;
+<<<<<<< Updated upstream
 
 	public float inBetweenShootTime = 2;
 
 	private float shootTimer = 0;
+=======
+	public GameObject bullet;
+	public float bulletSpeed = 1000f;
+	private bool isWithinTrigger;
+	public float fireRate = 2000f;
+	
+	private float shootTimer;
+>>>>>>> Stashed changes
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -18,6 +27,7 @@ public class BasicEnemyBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update()
 	{
+<<<<<<< Updated upstream
 		if (lastPlayerPos != null) {
 			shootTimer += Time.deltaTime * 1;
 			if (shootTimer % inBetweenShootTime == 0) {
@@ -26,6 +36,19 @@ public class BasicEnemyBehavior : MonoBehaviour {
 
 				// update to player's current position after the shoot has been made (if the player has moved since)
 				lastPlayerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+=======
+		if (isWithinTrigger) {
+			if(Time.time > shootTimer) {
+				Instantiate(bullet, transform.position, Quaternion.identity);
+				shootTimer = Time.time + fireRate / 1000;
+//				Vector2 lastPlayerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+//				Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y+2);
+
+//				Vector2 direction = enemyPos - lastPlayerPos;
+//				projectile.GetComponent<Rigidbody2D>().velocity = direction * 5;
+//				projectile.transform.position =
+//					Vector2.MoveTowards(projectile.transform.position, direction, bulletSpeed);
+>>>>>>> Stashed changes
 			}
 
 		}   
---
+++ GameJam/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+	private Rigidbody2D rb;
+	private float speed = 5f;
+	private GameObject player;
+	private Vector2 moveDirection;
+    // Start is called before the first frame update
+    void Start() {
+	    player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+        moveDirection = (player.transform.position - transform.position).normalized * speed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+//        Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+	    if (other.gameObject.CompareTag("Player")) {
+		    Debug.Log("WHAT");
+		    // FindObjectOfType<OxygenManager>().oxygenHit();
+		    Destroy(gameObject);
+	    }
+	    else if (!other.gameObject.CompareTag("Player")) {
+		    Destroy(gameObject);
+	    }
+
+    }
+}
