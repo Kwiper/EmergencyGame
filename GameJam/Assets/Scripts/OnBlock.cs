c1c35ecaec2ca1b30dc9b7cd82d7f002f097ee2f
Kybre <hermanchan10@gmail.com>
2021-05-03 02:04:37 -0700

    Switches + Module 2
    +added functionality for on/off switches
    + added Module 2

---
+++ GameJam/Assets/Scripts/OnBlock.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OnBlock : MonoBehaviour
+{
+    private bool recState = false; //recorded state, added to reduce load of script
+    private bool changed = false;
+    private Tilemap tilemap;
+    private Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        collider = GetComponent<CompositeCollider2D>();
+        collider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(recState != FindObjectOfType<Switch>().state)
+        {
+            changed = false;
+            recState = FindObjectOfType<Switch>().state;
+        }
+        if (!changed)
+        {
+            collider.isTrigger = !recState;
+            if (recState)
+            {
+                tilemap.color = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+                gameObject.layer = LayerMask.NameToLayer("Ground");
+            }
+            else
+            {
+                tilemap.color = new Color(0.0f, 1.0f, 0.0f, 0.5f);
+                gameObject.layer = LayerMask.NameToLayer("Inactive");
+            }
+            changed = true;
+        }
+    }
+}
