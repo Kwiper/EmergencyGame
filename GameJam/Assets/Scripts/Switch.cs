f41270a8ec660304ac00bba1052f740ae286b1c9
Benedict Wong <bwa44@sfu.ca>
2021-05-03 12:03:53 -0700

    Revert "Merge branch 'main' of https://github.com/Kwiper/EmergencyGame into main"
    This reverts commit 6e19c298319dca86bb659d23da12e784fd0fbc7b, reversing
    changes made to a5d08f31e6b62a2205d6c473f926c5037888449e.

--- GameJam/Assets/Scripts/Switch.cs
+++ GameJam/Assets/Scripts/Switch.cs
@@ -6,9 +6,8 @@ public class Switch : MonoBehaviour
 {
     private GameObject player;
     private bool inRange = false;
-    public bool state = false;
-    private bool toggled = false;
-    //public GameObject objectToChange;
+    private bool state = true;
+    public GameObject objectToChange;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,21 +23,19 @@ public class Switch : MonoBehaviour
     void OnTriggerExit2D(Collider2D other){
         if(other == player.GetComponent<Collider2D>()){
             inRange = false;
-            toggled = false;
         }
     }
     void Update(){
-        //bool enterIsPressed = Input.GetButtonDown("Submit");
-        if(inRange == true && toggled == false){
+        bool enterIsPressed = Input.GetButtonDown("Submit");
+        if(inRange == true && enterIsPressed == true){
             if(state == true){
-                //objectToChange.GetComponent<SpriteRenderer>().enabled=false;
+                objectToChange.GetComponent<SpriteRenderer>().enabled=false;
                 state = false;
             }
             else{
-                //objectToChange.GetComponent<SpriteRenderer>().enabled=true;
+                objectToChange.GetComponent<SpriteRenderer>().enabled=true;
                 state = true;
             }
-            toggled = true;
         }
     }
 }
