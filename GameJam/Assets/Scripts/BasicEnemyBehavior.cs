af7d23fa474e461be68943da98154fe9b9c5fa7c
Benedict Wong <bwa44@sfu.ca>
2021-05-02 22:16:01 -0700

    changes to bounding box for certain enemies

--- GameJam/Assets/Scripts/BasicEnemyBehavior.cs
+++ GameJam/Assets/Scripts/BasicEnemyBehavior.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 
@@ -22,18 +23,26 @@ public class BasicEnemyBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update()
 	{
+//		Vector2 lastPlayerPos = player.transform.position;
 		if (isWithinTrigger) {
-			if(Time.time > shootTimer) {
-				Instantiate(bullet, transform.position, Quaternion.identity);
-				shootTimer = Time.time + fireRate / 1000;
-//				Vector2 lastPlayerPos = new Vector2(player.transform.position.x, player.transform.position.y);
-//				Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y+2);
+//			RaycastHit2D hit =
+//				Physics2D.Raycast(transform.position, lastPlayerPos, 8);
+//			Debug.DrawRay(transform.position, GameObject.FindWithTag("Player").transform.position, Color.black);
+//			if (hit.collider == GameObject.FindWithTag("Player")) {
+				
+				Debug.Log("raycast has collided with player");
+				if (Time.time > shootTimer) {
+					Instantiate(bullet, transform.position, Quaternion.identity);
+					shootTimer = Time.time + fireRate / 1000;
+					//				Vector2 lastPlayerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+					//				Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y+2);
 
-//				Vector2 direction = enemyPos - lastPlayerPos;
-//				projectile.GetComponent<Rigidbody2D>().velocity = direction * 5;
-//				projectile.transform.position =
-//					Vector2.MoveTowards(projectile.transform.position, direction, bulletSpeed);
-			}
+					//				Vector2 direction = enemyPos - lastPlayerPos;
+					//				projectile.GetComponent<Rigidbody2D>().velocity = direction * 5;
+					//				projectile.transform.position =
+					//					Vector2.MoveTowards(projectile.transform.position, direction, bulletSpeed);
+				}
+//			}
 		}
 	}
 
