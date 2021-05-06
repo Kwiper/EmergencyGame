82e274efe9fdc314f8bfb58385739a7d6b97773d
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2021-05-05 17:53:04 -0700

    Title Screen Full Implementation
    -Added Levels to build queue
    -Buttons used to start the application
    -Buttons used to close the Application

---
+++ GameJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayButton : MonoBehaviour
+{
+    public void playGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
