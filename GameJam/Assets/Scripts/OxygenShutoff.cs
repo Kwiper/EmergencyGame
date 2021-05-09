1f72186a109b64a41fff7cc60ca46635f6083c52
Benedict Wong <bwa44@sfu.ca>
2021-05-09 12:56:39 -0700

    changed oxygen shutoff trigger to ontriggerenter to prevent players from encountering a bug

--- GameJam/Assets/Scripts/OxygenShutoff.cs
+++ GameJam/Assets/Scripts/OxygenShutoff.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OxygenShutoff : MonoBehaviour
 {
-    private void OnTriggerExit2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
 	    if (other.gameObject.CompareTag("Player")) {
             Debug.Log("I happen");
             // turn off oxygen counting down to prevent player from resetting to checkpoint and subsequently going to ending if player is low enough to die.
